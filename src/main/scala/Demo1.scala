cc477aba7d11b14839758ac2374fc044e98b4b9e
Alex <fava9410@hotmail.com>
2020-04-27 11:45:31 -0500

    delete a lot of things

--- src/main/scala/Demo1.scala
+++ src/main/scala/Demo1.scala
@@ -7,14 +7,14 @@ object Demo1 {
       .config("spark.master", "local")
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
-    val sdata = spark.read.textFile(sfile).cache()
+    /*val sdata = spark.read.textFile(sfile).cache()
 
     val numAs = sdata.filter(line => line.contains("a")).count()
     val numBs = sdata.filter(line => line.contains("b")).count()
     println(s"Lines with a: $numAs, Lines with b: $numBs")
 
     spark.stop()
-
+*/
     Actividad24
   }
 }
